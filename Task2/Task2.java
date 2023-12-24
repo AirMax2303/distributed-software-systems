bef2a3d071343d22a3aeea4e6f654bb3f674f364
AirMax2303 <max_hanter_77@mail.ru>
2023-12-24 23:06:34 +0300

    Initial commit

---
+++ Task2/Task2.java
@@ -0,0 +1,43 @@
+package Task1.Task2;
+
+import mpi.*;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        MPI.Init(args);
+
+        int rank = MPI.COMM_WORLD.Rank();
+
+        int size = MPI.COMM_WORLD.Size();
+
+        int[] sendBuffer = {rank};
+        int[] receiveBuffer = new int[1];
+
+        if(rank != 0) {
+            MPI.COMM_WORLD.Irecv(receiveBuffer,0, 1, MPI.INT, rank - 1, 0).Wait();
+            System.out.println("Процесс " + rank + " - "+ receiveBuffer[0]);
+
+            if (rank != 1) {
+                sendBuffer[0] += receiveBuffer[0];
+            }
+        }
+
+        if (rank == 0) {
+            // Производим обмен данными.
+            // Отправляем данные в следующий процесс и одновременно получаем с последнего процесса (блокирующий)
+            MPI.COMM_WORLD.Isend(sendBuffer, 0, 1, MPI.INT, (rank + 1) % size, 0);
+            MPI.COMM_WORLD.Irecv(receiveBuffer,0, 1, MPI.INT, size-1, 0).Wait();
+//            MPI.COMM_WORLD.Sendrecv(
+//                    sendBuffer, 0, 1, MPI.INT, (rank + 1) % size, 0,
+//                    receiveBuffer,0, 1, MPI.INT, size-1, 0
+//            );
+            System.out.println("Total sum: " + receiveBuffer[0]);
+        } else {
+            // Отправляем данные в след процесс (неблокирующий) 
+            MPI.COMM_WORLD.Isend(sendBuffer, 0, 1, MPI.INT, (rank + 1) % size, 0);
+        }
+
+        MPI.Finalize();
+    }
+}
